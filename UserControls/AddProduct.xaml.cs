52fa7b831072e29749fb8d5d9b3f5473b6a7c0e6
VaSuOk <vasuok15vas@gmail.com>
2021-04-06 00:08:18 +0300

    new control

--- UserControls/AddProduct.xaml.cs
+++ UserControls/AddProduct.xaml.cs
@@ -33,48 +33,132 @@ namespace Client_MilkAndMeat.UserControls
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            try
+            if (NameProductText.Text == "" || NameProductText.Text == "Input name")
             {
-                if (imageName != "")
+                LogBar.Content = "Введіть назву продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BName.Background = Brushes.Red;
+            }
+            else if (DescriptionText.Text == "" || DescriptionText.Text == "Input Description")
+            {
+                LogBar.Content = "Введіть опис продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BDescription.Background = Brushes.Red;
+            }
+            else if (SortText.Text == "" || SortText.Text == "input sort")
+            {
+                LogBar.Content = "Введіть сорт продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BSort.Background = Brushes.Red;
+            }
+            else if (GroupText.Text == "" || GroupText.Text == "Input group")
+            {
+                LogBar.Content = "Введіть групу продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BGroup.Background = Brushes.Red;
+            }
+            else if (imageName == "" || imageName == null)
+            {
+                LogBar.Content = "Оберіть фотографію продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BImage.Background = Brushes.Red;
+            }
+            else if (PriceText.Text == "" || PriceText.Text == "0")
+            {
+                LogBar.Content = "Введіть ціну продукту!";
+                LogBar.Visibility = Visibility.Visible;
+                BPrice.Background = Brushes.Red;
+            }
+            else
+            {
+                try
                 {
-                    //Initialize a file stream to read the image file
-                    FileStream fs = new FileStream(imageName, FileMode.Open, FileAccess.Read);
-
-                    //Initialize a byte array with size of stream
-                    byte[] imgByteArr = new byte[fs.Length];
-
-                    //Read data from the file stream and put into the byte array
-                    fs.Read(imgByteArr, 0, Convert.ToInt32(fs.Length));
-
-                    //Close a file stream
-                    fs.Close();
-
-                    DataBase.Get_Instance().Connect();
-                    MySqlCommand command = new MySqlCommand(
-                                "INSERT INTO `product` (`ID_manufacturer`, `Name`, `Description`, `Sort`, `Groupe`, `Image`, `Price`) " +
-                                "VALUES ( @ID_manufacturer, @Name, @Description, @Sort, @Groupe, @Image, @Price)", DataBase.Get_Instance().connection);
-
-                    command.Parameters.Add("@ID_manufacturer", MySqlDbType.Int32).Value = ID_man;
-                    command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = NameProductText.Text;
-                    command.Parameters.Add("@Description", MySqlDbType.VarChar).Value = DescriptionText.Text;
-                    command.Parameters.Add("@Sort", MySqlDbType.VarChar).Value = SortText.Text;
-                    command.Parameters.Add("@Groupe", MySqlDbType.VarChar).Value = GroupText.Text;
-                    command.Parameters.Add("@Image", MySqlDbType.MediumBlob).Value = imgByteArr;
-                    command.Parameters.Add("@Price", MySqlDbType.Double).Value = Convert.ToDouble(PriceText.Text);
-
-                    if (command.ExecuteNonQuery() > 0)
+                    if (imageName != "")
                     {
+                        //Initialize a file stream to read the image file
+                        FileStream fs = new FileStream(imageName, FileMode.Open, FileAccess.Read);
+
+                        //Initialize a byte array with size of stream
+                        byte[] imgByteArr = new byte[fs.Length];
+
+                        //Read data from the file stream and put into the byte array
+                        fs.Read(imgByteArr, 0, Convert.ToInt32(fs.Length));
+
+                        //Close a file stream
+                        fs.Close();
+
+                        DataBase.Get_Instance().Connect();
+                        MySqlCommand command = new MySqlCommand(
+                                    "INSERT INTO `product` (`ID_manufacturer`, `Name`, `Description`, `Sort`, `Groupe`, `Image`, `Price`) " +
+                                    "VALUES ( @ID_manufacturer, @Name, @Description, @Sort, @Groupe, @Image, @Price)", DataBase.Get_Instance().connection);
+
+                        command.Parameters.Add("@ID_manufacturer", MySqlDbType.Int32).Value = ID_man;
+                        command.Parameters.Add("@Name", MySqlDbType.VarChar).Value = NameProductText.Text;
+                        command.Parameters.Add("@Description", MySqlDbType.VarChar).Value = DescriptionText.Text;
+                        command.Parameters.Add("@Sort", MySqlDbType.VarChar).Value = SortText.Text;
+                        command.Parameters.Add("@Groupe", MySqlDbType.VarChar).Value = GroupText.Text;
+                        command.Parameters.Add("@Image", MySqlDbType.MediumBlob).Value = imgByteArr;
+                        command.Parameters.Add("@Price", MySqlDbType.Double).Value = Convert.ToDouble(PriceText.Text);
+
+                        if (command.ExecuteNonQuery() > 0)
+                        {
+                            LogBar.Content = "Дані успішно занесені !";
+                            LogBar.Foreground = Brushes.Green;
+                            LogBar.Visibility = Visibility.Visible;
+                        }
+                        else
+                        {
+                            LogBar.Content = "Щось пішло не так, дані не були занесені !";
+                            LogBar.Foreground = Brushes.Red;
+                            LogBar.Visibility = Visibility.Visible;
+                        }
 
 
                     }
-
-
+                }
+                catch (Exception ex)
+                {
+                    LogBar.Content = "Відсутнє з'єднання з БД, дані не були занесені !";
+                    LogBar.Foreground = Brushes.Red;
+                    LogBar.Visibility = Visibility.Visible;
+                    //MessageBox.Show(ex.Message);
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        }
+
+        private void PriceText_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "0123456789,".IndexOf(e.Text) < 0;
+        }
+
+        private void NameProductText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BName.Background == Brushes.Red) { BName.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void DescriptionText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BDescription.Background == Brushes.Red) { BDescription.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void SortText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BSort.Background == Brushes.Red) { BSort.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void GroupText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BGroup.Background == Brushes.Red) { BGroup.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void Button_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BImage.Background == Brushes.Red) { BImage.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void PriceText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BPrice.Background == Brushes.Red) { BPrice.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -89,7 +173,10 @@ namespace Client_MilkAndMeat.UserControls
                     strName = fldlg.SafeFileName;
                     imageName = fldlg.FileName;
                     ImageSourceConverter isc = new ImageSourceConverter();
-                    img.SetValue(Image.SourceProperty, isc.ConvertFromString(imageName));
+                    ImageBrush imgbr = new ImageBrush();
+                    imgbr.ImageSource = new BitmapImage(new Uri(imageName, UriKind.Absolute));
+                    Bimg.Background = imgbr;
+                    
                 }
                 fldlg = null;
             }
--- UserControls/UserProfileControl.xaml.cs
+++ UserControls/UserProfileControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Client_MilkAndMeat.Users;
+﻿using Client_MilkAndMeat.DB;
+using Client_MilkAndMeat.Users;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -31,6 +33,7 @@ namespace Client_MilkAndMeat.UserControls
             NameText.Text = (mainManufacturer.PIB).Split(' ')[1];
             SurnameText.Text = (mainManufacturer.PIB).Split(' ')[0];
             positionText.Text = mainManufacturer.Position;
+            positionText.IsReadOnly = true;
             EmailText.Text = mainManufacturer.Email;
             PhoneText.Text = mainManufacturer.Phone;
 
@@ -40,6 +43,186 @@ namespace Client_MilkAndMeat.UserControls
             RegionText.Text = mainManufacturer.Region;
             DirectorText.Text = mainManufacturer.PIBManager;
             PhoneDirectorText.Text = mainManufacturer.Manager_Phone;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameText.Text == "")
+            {
+                LogBar.Content = "Введіть ім'я!";
+                LogBar.Visibility = Visibility.Visible;
+                BName.Background = Brushes.Red;
+            }
+            else if (SurnameText.Text == "")
+            {
+                LogBar.Content = "Введіть прізвище!";
+                LogBar.Visibility = Visibility.Visible;
+                BSurname.Background = Brushes.Red;
+            }
+            else if (EmailText.Text == "")
+            {
+                LogBar.Content = "Введіть  дані пошти!";
+                LogBar.Visibility = Visibility.Visible;
+                BEmail.Background = Brushes.Red;
+            }
+            else if (PhoneText.Text == "")
+            {
+                LogBar.Content = "Введіть мобільний номер!";
+                LogBar.Visibility = Visibility.Visible;
+                BPhone.Background = Brushes.Red;
+            }
+            else if (NameCompanyText.Text == "")
+            {
+                LogBar.Content = "Введіть назву компанії!";
+                LogBar.Visibility = Visibility.Visible;
+                BCompanyName.Background = Brushes.Red;
+            }
+            else if (DescriptionText.Text == "")
+            {
+                LogBar.Content = "Введіть опис компанії!";
+                LogBar.Visibility = Visibility.Visible;
+                BDescription.Background = Brushes.Red;
+            }
+            else if (AddresText.Text == "")
+            {
+                LogBar.Content = "Введіть адресу компанії!";
+                LogBar.Visibility = Visibility.Visible;
+                BAddres.Background = Brushes.Red;
+            }
+            else if (RegionText.Text == "")
+            {
+                LogBar.Content = "Введіть область розташування компанії!";
+                LogBar.Visibility = Visibility.Visible;
+                BRegion.Background = Brushes.Red;
+            }
+            else if (DirectorText.Text == "")
+            {
+                LogBar.Content = "Введіть ініціали директора!";
+                LogBar.Visibility = Visibility.Visible;
+                BDirector.Background = Brushes.Red;
+            }
+            else if (PhoneDirectorText.Text == "")
+            {
+                LogBar.Content = "Введіть мобільний номер директора!";
+                LogBar.Visibility = Visibility.Visible;
+                BPhoneDir.Background = Brushes.Red;
+            }
+            else
+            {
+                ChangeData();
+            }
+         
+        }
+        private void ChangeData()
+        {
+            mainManufacturer.PIB = NameText.Text + " " + SurnameText.Text;
+            mainManufacturer.Phone = PhoneText.Text;
+            mainManufacturer.Email = EmailText.Text;
+
+            mainManufacturer.Address = AddresText.Text;
+            mainManufacturer.Region = RegionText.Text;
+            mainManufacturer.PIBManager = DirectorText.Text;
+            mainManufacturer.NameCompany = NameCompanyText.Text;
+            mainManufacturer.Description = DescriptionText.Text;
+            mainManufacturer.Manager_Phone = PhoneDirectorText.Text;
+
+            try
+            {
+                DataBase.Get_Instance().Connect();
+                MySqlCommand command = new MySqlCommand(
+                            "UPDATE `user` SET `PIB` = @PIB, `Email` = @Email, `Mobile_number` = @Mobile_number " +
+                            "WHERE `ID` = @ID", DataBase.Get_Instance().connection);
+                command.Parameters.Add("@ID", MySqlDbType.Int32).Value = mainManufacturer.id;
+                command.Parameters.Add("@PIB", MySqlDbType.VarChar).Value = mainManufacturer.PIB;
+                command.Parameters.Add("@Email", MySqlDbType.VarChar).Value = mainManufacturer.Email;
+                command.Parameters.Add("@Mobile_number", MySqlDbType.VarChar).Value = mainManufacturer.Phone;
+
+                MySqlCommand command2 = new MySqlCommand(
+                            "UPDATE `manufacturer` SET `Address` = @Address, `Region` = @Region, `Mobile_number` = @Mobile_number," +
+                            " `Manager` = @Manager, `Name_company` = @NameCompany, `Description` = @Description " +
+                            "WHERE `ID` = @ID", DataBase.Get_Instance().connection);
+                command2.Parameters.Add("@ID", MySqlDbType.Int32).Value = mainManufacturer.ID_Manufacturer;
+                command2.Parameters.Add("@Address", MySqlDbType.VarChar).Value = mainManufacturer.Address;
+                command2.Parameters.Add("@Region", MySqlDbType.VarChar).Value = mainManufacturer.Region;
+                command2.Parameters.Add("@Mobile_number", MySqlDbType.VarChar).Value = mainManufacturer.Phone;
+                command2.Parameters.Add("@Manager", MySqlDbType.VarChar).Value = mainManufacturer.PIBManager;
+                command2.Parameters.Add("@NameCompany", MySqlDbType.VarChar).Value = mainManufacturer.NameCompany;
+                command2.Parameters.Add("@Description", MySqlDbType.VarChar).Value = mainManufacturer.Description;
+
+                if (command.ExecuteNonQuery() > 0 && command2.ExecuteNonQuery()>0)
+                {
+                    LogBar.Foreground = Brushes.Green;
+                    LogBar.Content = "Зміни успішно збережені!";
+                    LogBar.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    LogBar.Foreground = Brushes.Red;
+                    LogBar.Content = "Виникла помилка, зміни не були занесені!";
+                    LogBar.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception e)
+            {
+                LogBar.Foreground = Brushes.Red;
+                LogBar.Content = "Відсутнє з'єднання з БД!";
+                LogBar.Visibility = Visibility.Visible;
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void NameCompanyText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if(BCompanyName.Background == Brushes.Red) { BCompanyName.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void DescriptionText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BDescription.Background == Brushes.Red) { BDescription.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void AddresText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BAddres.Background == Brushes.Red) { BAddres.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void RegionText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BRegion.Background == Brushes.Red) { BRegion.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void DirectorText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BDirector.Background == Brushes.Red) { BDirector.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void PhoneDirectorText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BPhone.Background == Brushes.Red) { BPhone.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void NameText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BName.Background == Brushes.Red) { BName.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void SurnameText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BSurname.Background == Brushes.Red) { BSurname.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void positionText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BPosition.Background == Brushes.Red) { BPosition.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void EmailText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BEmail.Background == Brushes.Red) { BEmail.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
+        }
+
+        private void PhoneText_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (BPhone.Background == Brushes.Red) { BPhone.Background = new SolidColorBrush(Color.FromRgb(8, 17, 50)); LogBar.Visibility = Visibility.Hidden; }
         }
     }
 }
